5fdd43ab16848a03a8023c4da929d9c70ff90854
Sriteja Somepalli <sritejamahadevan@gmail.com>
2024-07-01 15:40:53 +0200

    Restructures the entire files and folders
    1. The components are added separately inside source folder
    2. Font changes are added globally inside index.html

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./App.module.css";
+
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <h1>Ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BusinessList() {
+  return (
+    <div>BusinessList</div>
+  )
+}
+
+export default BusinessList
